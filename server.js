9f6b8007676eeab1aac0601c2f7f34ab1b44f6a5
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-20 11:30:08 +0100

    csrf tunning

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/city-locator-ui'));
 
-app.get('/*', function(req,res) {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
+app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/city-locator-ui/index.html'));
 });
 
